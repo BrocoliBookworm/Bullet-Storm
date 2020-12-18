b8da1ced540ff81451edaacc614ef4151271454e
BrocoliBookworm <brandonebcharles@gmail.com>
2020-12-18 11:14:00 -0800

    Fixed UI Issues and Put in Flash Start

---
+++ EmergencyRescue/Assets/Scripts/UI/ButtonSetter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSetter : MonoBehaviour
+{
+    public float timer;
+
+    public GameObject playButton;
+
+    public GameObject backButton;
+
+    void Start()
+    {
+        timer = 20f;
+        playButton = GameObject.Find("PlayButton");
+        backButton = GameObject.Find("BackButton (1)");
+        playButton.SetActive(false);
+        backButton.SetActive(false);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            playButton.SetActive(true);
+            backButton.SetActive(true);
+        }
+    }
+
+
+}
---
+++ EmergencyRescue/Assets/Scripts/UI/Wait.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+    public float waitTime;
+
+    void Start()
+    {
+        StartCoroutine(Waiting());
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
