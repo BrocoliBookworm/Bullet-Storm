8a92f1d19c42ce09c3a040a8d12b9a075703e3dc
BrocoliBookworm <brandonebcharles@gmail.com>
2020-12-18 12:07:20 -0800

    Fixed UI Bugs Found While Testing

--- EmergencyRescue/Assets/Scripts/UI/ButtonSetter.cs
+++ EmergencyRescue/Assets/Scripts/UI/ButtonSetter.cs
@@ -10,9 +10,14 @@ public class ButtonSetter : MonoBehaviour
 
     public GameObject backButton;
 
+    public bool wentBack = false;
+
+    public bool goForwards = false;
+
     void Start()
     {
         timer = 20f;
+
         playButton = GameObject.Find("PlayButton");
         backButton = GameObject.Find("BackButton (1)");
         playButton.SetActive(false);
@@ -30,5 +35,28 @@ public class ButtonSetter : MonoBehaviour
         }
     }
 
+    public void Backer()
+    {
+        if(!wentBack)
+        {
+            wentBack = true;
+            goForwards = false;
+        }
+    }
 
+    public void Forwards()
+    {
+        if(!goForwards)
+        {
+            goForwards = true;
+            wentBack = false;
+        }
+
+        if(goForwards)
+        {
+            timer = 20f;
+            playButton.SetActive(false);
+            backButton.SetActive(false);
+        }
+    }
 }
